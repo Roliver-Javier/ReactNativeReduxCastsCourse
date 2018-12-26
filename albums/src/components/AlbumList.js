0846622931427e3dd95107b40fa82bc4383f8230
RoliverJavier <roliverjavier@gmail.com>
2018-12-26 14:02:38 -0400

    Chapter 37
    Rendering a List of Components

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {ScrollView, Text} from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component{
+    state = { albums : []};
+
     componentWillMount(){
-        console.log('hola');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then((response)=>{
+            this.setState({albums: response.data});
+        });
+    }
+
+    renderAlbums(){
+        this.state.albums.map(album =>{
+            // <AlbumDetail key={album.title} album={album} />
+            <Text>{album.title}</Text>
+        });
     }
     
     render(){
         return(
-            <View>
-                <Text>Album List</Text>
-            </View>
+            <ScrollView>
+                {this.renderAlbums()}
+            </ScrollView>
         );
     }
 }
