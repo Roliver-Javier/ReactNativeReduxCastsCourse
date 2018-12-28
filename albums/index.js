6f466f287bf9eb95f945b3cc7a70f68f131e12b1
RoliverJavier <roliverjavier@gmail.com>
2018-12-27 23:24:31 -0400

    handling component layout
    lessons from section 8 of complete react native and redux course

--- albums/index.js
+++ albums/index.js
@@ -6,7 +6,7 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 const App = () =>(
-    <View>
+    <View style={{flex: 1}}>
        <Header title="Albums"></Header>
        <AlbumList></AlbumList>
     </View>
--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import {ScrollView, Text} from 'react-native';
+import {ScrollView} from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
 
 class AlbumList extends Component{
     state = { albums : []};
@@ -13,10 +15,9 @@ class AlbumList extends Component{
     }
 
     renderAlbums(){
-        this.state.albums.map(album =>{
-            // <AlbumDetail key={album.title} album={album} />
-            <Text>{album.title}</Text>
-        });
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />
+        );
     }
     
     render(){
